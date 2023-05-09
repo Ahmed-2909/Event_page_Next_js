606a058f0e679a4d47dc79b3637ffb365e3992e5
Ahmed <kaziahmed.r@solutelabs.com>
2023-05-09 16:52:37 +0530

    some Change

--- component/layout/Mai-layout.jsx
+++ component/layout/Mai-layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Footer } from "../footer/footer";
-import { Header } from "../header/header";
+import { Footer } from "../footer/Footer";
+import { Header } from "../header/Header";
 
 const MainLayout = ({ children }) => {
   return (
